5daba61e6a1720a5f7df39bb60572dd647d85bd4
Owen <84158375+stkin1st@users.noreply.github.com>
2025-11-01 22:44:11 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,14 @@
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const fs = require('fs');
-
-// Import filters
 const dateFilter = require('./src/filters/date-filter.js');
 const markdownFilter = require('./src/filters/markdown-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
-
-// Import transforms
 const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 const parseTransform = require('./src/transforms/parse-transform.js');
-
-// Import data files
 const site = require('./src/_data/site.json');
 
-module.exports = function(config) {
+module.exports = function (config) {
   // Filters
   config.addFilter('dateFilter', dateFilter);
   config.addFilter('markdownFilter', markdownFilter);
@@ -36,23 +30,7 @@ module.exports = function(config) {
   config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
   config.addPassthroughCopy('src/robots.txt');
 
-  const now = new Date();
-
-  // Custom collections
-  const livePosts = post => post.date <= now && !post.data.draft;
-  config.addCollection('posts', collection => {
-    return [
-      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
-    ].reverse();
-  });
-
-  config.addCollection('postFeed', collection => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
-      .reverse()
-      .slice(0, site.maxPostsPerPage);
-  });
-
-  // ✅ Add bridge pages collection here (INSIDE)
+  // ✅ Add Bridge pages
   config.addCollection('bridges', collection => {
     return collection.getFilteredByGlob('src/bridges/*.md');
   });
@@ -64,21 +42,18 @@ module.exports = function(config) {
   // 404
   config.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
+      ready: function (err, browserSync) {
         const content_404 = fs.readFileSync('dist/404.html');
         browserSync.addMiddleware('*', (req, res) => {
           res.write(content_404);
           res.end();
         });
-      }
-    }
+      },
+    },
   });
 
   return {
-    dir: {
-      input: 'src',
-      output: 'dist'
-    },
-    passthroughFileCopy: true
+    dir: { input: 'src', output: 'dist' },
+    passthroughFileCopy: true,
   };
 };
